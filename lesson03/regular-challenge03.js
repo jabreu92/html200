53726a434d418dce85bbebc67a2fbc258adc714e
jabreu92 <jose.abreu4@hotmail.com>
2020-11-03 17:38:32 -0800

    Adding regular challenge 3 program !

--- lesson03/regular-challenge03.js
+++ lesson03/regular-challenge03.js
@@ -1,6 +1,7 @@
 /** Regular Challenge 3 Program **/
 let array = ['cats','dogs','mice','horses','birds'];
 let index = 1;
+
 array.forEach( function(element) {
   console.log(`${index},${element}`);
   index++;
